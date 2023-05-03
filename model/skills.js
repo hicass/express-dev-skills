cd12b7ff824eaf70d5773506cf143adc72801870
hicass <cassidyjwalters@gmail.com>
2023-05-02 17:09:02 -0700

    Initial Express skeleton with starting skill data.

---
+++ model/skills.js
@@ -0,0 +1,8 @@
+const skills = [
+    {id:415333, skill: 'HTML'},
+    {id:415332, skill: 'CSS'},
+    {id:415331, skill: 'JavaScript'},
+    {id:415330, skill: 'Node.js'},
+    {id:415329, skill: 'Express'}
+];
+
